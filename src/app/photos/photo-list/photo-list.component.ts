7e8abd10bd210cb081d53c2fb9b87a0c593f139f
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 13:57:31 +0200

    using path parameters

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-photo-list',
@@ -11,10 +12,16 @@ export class PhotoListComponent implements OnInit {
 
   photos: Photo[] = [];
   
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+    ) {}
     
   ngOnInit(): void {
-    const observable = this.photoService.listFromUser('flavio');  
+
+    const userName = this.activatedRoute.snapshot.params.userName;
+
+    const observable = this.photoService.listFromUser(userName);  
     
     observable.subscribe(
       photos => 
--- src/app/photos/photo/photo.service.ts
+++ src/app/photos/photo/photo.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Photo } from "./photo";
 
-const API = "http://localhost:3000/";
+const API = "http://localhost:3000";
 
 @Injectable({ providedIn: 'root'})
 export class PhotoService{
@@ -10,6 +10,6 @@ export class PhotoService{
     constructor(private http: HttpClient){}
 
     listFromUser(userName: string){
-        return this.http.get<Photo[]>(API + "flavio/photos");
+        return this.http.get<Photo[]>(API + "/" + userName + "/photos");
     }
 }
