ca2d8240be8fa8ba4f7f9217a682e4f5ed62543b
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 02:01:43 +0200

    first component created / component imported

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'wp-photo',
+    templateUrl: 'photo.component.html'
+})
+export class PhotoComponent {
+    description = 'Logo'
+    url = 'https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA5Ny8wNzQvb3JpZ2luYWwvbW9ua2V5LWNvdW50aW5nLmpwZw=='  
+}
