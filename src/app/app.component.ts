6379a505cab58b6b911ba912cc652f07af555862
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 12:09:15 +0200

    server installed / httpClient added / observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Waser Pics';
- }
+  photos: Object[] = [];
+  
+  constructor(http: HttpClient) {
+    console.log(http);
+
+    const observable = http.get<Object[]>("http://localhost:3000/flavio/photos");
+    observable.subscribe(
+        photos => this.photos = photos,
+        err => console.log(err.message)
+      );
+  }
+}
