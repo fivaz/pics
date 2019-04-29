452f384e843e9f655da0e9d9dda27e490f94ffaf
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 22:14:37 +0200

    fea my own hover with Renderer setElementStyle

--- src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
+++ src/app/shared/directives/darken-on-hover/darken-on-hover.directive.ts
@@ -1,19 +1,21 @@
-import { Directive, ElementRef, HostListener } from '@angular/core';
+import { Directive, ElementRef, HostListener, Renderer } from '@angular/core';
 
 @Directive({
   selector: '[wpDarkenOnHover]'
 })
 export class DarkenOnHoverDirective {
 
-  constructor(private el:ElementRef) { }
+  constructor(
+    private el:ElementRef,
+    private render: Renderer) { }
 
   @HostListener('mouseover')
   darkenOn(){
-    console.log("darkenOn");
+    this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(70%)');
   }
 
   @HostListener('mouseleave')
   darkenOff(){
-    console.log("darkenOff");
+    this.render.setElementStyle(this.el.nativeElement, 'filter', 'brightness(100%)');
   }
 }
