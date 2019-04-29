f5c87dc0b350bf60074342bc78a66cbcf9c6a837
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 20:51:19 +0200

    shared compoenents / ng-content

--- src/app/photos/photo-list/photo-list.module.ts
+++ src/app/photos/photo-list/photo-list.module.ts
@@ -6,11 +6,13 @@ import { PhotosComponent } from './photos/photos.component';
 import { LoadButtonComponent } from './load-button/load-button.component';
 import { FilterByDescription } from './filter-by-description.pipe';
 import { PhotoModule } from '../photo/photo.module';
+import { CardModule } from 'src/app/shared/components/card/card.module';
 
 @NgModule({
   imports: [ 
     CommonModule,
-    PhotoModule
+    PhotoModule,
+    CardModule
   ],
   declarations: [
     PhotoListComponent,
