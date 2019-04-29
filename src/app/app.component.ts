fd5fff994f3b3da1ecce62820c72fe049c996d47
Stefane Fivaz <elv-stefane-pntsf@eduge.ch>
2019-04-29 12:48:45 +0200

    interface onInit implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { PhotoService } from './photos/photo/photo.service';
+import { Photo } from './photos/photo/photo';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Waser Pics';
-  photos: any[] = [];
+  photos: Photo[] = [];
   
-  constructor(photoService: PhotoService) {
-
-    const observable = photoService.listFromUser('flavio');
-
+  constructor(private photoService: PhotoService) {}
+    
+  ngOnInit(): void {
+    const observable = this.photoService.listFromUser('flavio');  
+    
     observable.subscribe(
       photos => 
-        this.photos = photos,
+      this.photos = photos,
       err => console.log(err.message)
     );
   }
